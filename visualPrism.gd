283af75f6e090d67d1368f9905990f7a72ca6798
Andrew Pritchard <andrewjpritchard@gmail.com>
2018-08-02 22:27:47 +0800

    Playing with surfaceTool

--- visualPrism.gd
+++ visualPrism.gd
@@ -1,6 +1,8 @@
 extends MeshInstance
 
-export var height_variance = 0.5
+export var height_variance = 2
+
+var surface = SurfaceTool.new()
 
 # class member variables go here, for example:
 # var a = 2
@@ -12,19 +14,56 @@ export var height_variance = 0.5
 	# pass
 	
 func draw(polygon, centre):
+	var mesh = Mesh.new()
 	var height = randf() * height_variance
 	
-	var surface = SurfaceTool.new()
-	var mesh = Mesh.new()
-	
 	var mat = SpatialMaterial.new()
-	mat.flags_unshaded = true;
+	# mat.flags_unshaded = true;
 	mat.albedo_color = Color(randf(), randf(), randf())
 	# mat.albedo_texture = load("res://icon.png");
   
-	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
-	surface.set_material(mat)
+	drawHorizontalPolygon(polygon, centre, height, mat, mesh)
+	drawHorizontalPolygon(polygon, centre, 0, mat, mesh)
 	
+	for i in range (polygon.size()):
+		var v = polygon[i]
+		var v2 = polygon[(i + 1) % polygon.size()]
+		
+		drawVerticalSide (v, v2, 0, height, mat, mesh)
+  
+	self.mesh = mesh
+
+func drawVerticalSide (v1, v2, h0, h1, material, mesh):
+	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+	surface.set_material(material)
+		
+	surface.add_uv(Vector2(0, 0))
+	surface.add_vertex(Vector3(v1.x, h0, v1.y))
+		
+	surface.add_uv(Vector2(0, 1))
+	surface.add_vertex(Vector3(v1.x, h1, v1.y))
+		
+	surface.add_uv(Vector2(1, 1))
+	surface.add_vertex(Vector3(v2.x, h1, v2.y))
+	
+	surface.add_uv(Vector2(0, 0))
+	surface.add_vertex(Vector3(v1.x, h0, v1.y))
+		
+	surface.add_uv(Vector2(1, 1))
+	surface.add_vertex(Vector3(v2.x, h1, v2.y))
+		
+	surface.add_uv(Vector2(1, 0))
+	surface.add_vertex(Vector3(v2.x, h0, v2.y))
+  
+	surface.generate_normals()
+	surface.index()
+  
+	surface.commit(mesh)
+
+func drawHorizontalPolygon (polygon, centre, height, material, mesh):
+	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+	surface.set_material(material)
+		
 	for i in range (polygon.size()):
 		var v = polygon[i]
 		var v2 = polygon[(i + 1) % polygon.size()]
@@ -42,8 +81,6 @@ func draw(polygon, centre):
 	surface.index()
   
 	surface.commit(mesh)
-  
-	self.mesh = mesh
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
